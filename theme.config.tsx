3e5176ef0b41313d21cf58d3da0c80813bd65b78
nzai <nzaih18@gmail.com>
2023-07-22 20:15:00 +0300

    fix some stuff

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
+  
   logo: <span className="md:text-3xl font-bold ">Atomize Icons</span>,
   project: {
     link: "https://github.com/nzaih1999/atomize_icons",
